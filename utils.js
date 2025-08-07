5f27d9dca8fe7527150f076af4a9fa117da6802d
Dimitri Viana Pedroni <135239573+Dimitri-VP@users.noreply.github.com>
2025-08-07 09:00:50 -0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -5,27 +5,31 @@ async function readExcel(filePath) {
   await workbook.xlsx.readFile(filePath);
   const worksheet = workbook.getWorksheet(1);
   const data = [];
-  const headers = [];
-  
-  worksheet.getRow(1).eachCell((cell) => {
-    headers.push(cell.value);
-  });
 
   worksheet.eachRow((row, rowNumber) => {
-    if (rowNumber > 1) {
-      const rowData = {};
-      row.eachCell((cell, colNumber) => {
-        rowData[headers[colNumber - 1]] = cell.value;
+    if (rowNumber > 1) { // Ignora a primeira linha (cabeçalho)
+      data.push({
+        Data: row.getCell(1).value,
+        TipoBaixa: row.getCell(2).value,
+        Empresa: row.getCell(3).value,
+        ContaCorrente: row.getCell(4).value,
+        Titulo: row.getCell(5).value,
+        Parcela: row.getCell(6).value,
+        ParcialTotal: row.getCell(7).value,
+        Valor: row.getCell(8).value,
+        CorrecaoMonetaria: row.getCell(9).value,
+        Juros: row.getCell(10).value,
+        Multa: row.getCell(11).value,
+        ExecutarOpcional: row.getCell(12).value
       });
-      data.push(rowData);
     }
   });
+
   return data;
 }
 
-async function simulateHumanDelay(min = 1000, max = 3000) {
-  const delay = Math.floor(Math.random() * (max - min + 1)) + min;
-  await new Promise(resolve => setTimeout(resolve, delay));
+function simulateHumanDelay() {
+  return new Promise(resolve => setTimeout(resolve, Math.random() * 2000 + 1000));
 }
 
 module.exports = { readExcel, simulateHumanDelay };
