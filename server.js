42946cd6ae9e73b49661a4ae20943120dd9533ac
Dimitri Viana Pedroni <135239573+Dimitri-VP@users.noreply.github.com>
2025-08-07 02:25:38 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const multer = require('multer');
 const path = require('path');
 const { readExcel } = require('./utils');
 const { processPayments } = require('./automation');
+const { generateExcelTemplate } = require('./generateTemplate');
 
 const app = express();
 const upload = multer({ dest: 'uploads/' });
@@ -11,6 +12,15 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/download-template', async (req, res) => {
+  try {
+    const filePath = await generateExcelTemplate();
+    res.download(filePath, 'template.xlsx');
+  } catch (error) {
+    res.status(500).json({ success: false, message: `Erro ao gerar template: ${error.message}` });
+  }
 });
 
 app.post('/upload', upload.single('excelFile'), async (req, res) => {
